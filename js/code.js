2174b78fd35963d0cccd6abc315f14ba0db84944
Dan1elH3ctor <danielhector080@gmail.com>
2022-11-30 16:30:47 +0200

    update

--- js/code.js
+++ js/code.js
@@ -1,6 +1,17 @@
-function personalDetails(firstName, lastName, email){
-    this.firstName =  firstName;
-    this.lastName = lastName;
-    this.email = email;
-}
+// function personalDetails(firstName, lastName, email){
+//     this.firstName =  firstName;
+//     this.lastName = lastName;
+//     this.email = email;
+// }
 
+
+
+
+
+// form
+function addition(e){
+    e.preventDefault();
+    let numb1 = document.querySelector("#numb1").value;
+    let numb2 = document.querySelector("#numb2").value;
+    let output = document.querySelector("#output").innerText = eval(`${numb1} + ${numb2}`);
+}
